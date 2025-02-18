a04ed3b10152586751c3a1c32620c176eca8d380
david <david@WAFFLEFRAME>
2025-02-17 16:11:43 -0800

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Wake_On_Lan
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
